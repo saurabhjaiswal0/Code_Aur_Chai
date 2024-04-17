9f49e295306545f835d7fc8da1628e8e0c8144d6
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-04-17 18:02:34 +0000

    container config done

---
+++ Basics/Datatype.js
@@ -0,0 +1,8 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3)// we are using node js ,not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof "hitesh")
