fb23ecac61ea832a211850dad7a16456fd1f193b
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-04-18 13:37:40 +0000

    Array

---
+++ Basics_01/Array.js
@@ -0,0 +1,29 @@
+//Array
+
+const myArr = [0,1,2,3,4,true,"Hitesh"]
+
+// console.log(myArr[0])
+
+//Array Methods
+
+// myArr.push(0)
+// myArr.push(7)
+// console.log(myArr);
+
+// myArr.unshift(5)
+// myArr.shift()
+// console.log(myArr)
+
+// console.log(myArr.includes(9))
+
+// slice, splice
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1)
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log(myn2)
+
