5ca8d36ccfdc59e56d584334360bcad1cb277ab2
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-04 05:38:13 +0000

    Math And Num

---
+++ Basics_01/Num_and_math.js
@@ -0,0 +1,28 @@
+const score= 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+// ***************************MATH************************************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(1,2,5,3));
+// console.log(Math.max(1,2,5,3));
+
+console.log(Math.random());
+// Math.random() give the value of 0 to 1.....
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
