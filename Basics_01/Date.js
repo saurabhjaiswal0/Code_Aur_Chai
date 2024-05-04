546077ac13a27b8d26f69dd0a1db01c2418e79a6
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-04 06:13:53 +0000

    Date

---
+++ Basics_01/Date.js
@@ -0,0 +1,24 @@
+// Date*****************************
+
+
+// let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate=new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date("01-14-2024")
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
+newDate.toLocaleString('default0',{
+    Weekday:"long",
+})
