0a2ddce3ebf330867388d01939bfc57d289b3386
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-04 02:38:59 +0000

    Datatype

---
+++ Basics_01/datatype.js
@@ -0,0 +1,30 @@
+// Primitive Data type //
+
+// 7 type :String, Number, Boolean, Null, Undefined, Symbol,BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outSideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+
+
+// Refrence Type(Non Primitive)
+//  Array ,Objects,Function
+
+const heros =["Shaktiman","Nagraj","Doga"]//Array
+let myObj={
+    name:Saurabh,
+    age:26,
+
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+}
