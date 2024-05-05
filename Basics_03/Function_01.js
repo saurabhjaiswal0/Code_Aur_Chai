f3b798d1b97d09b5229f63e7760a999112f3b241
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-05 13:36:18 +0000

    function

---
+++ Basics_03/Function_01.js
@@ -0,0 +1,25 @@
+console.log("H");
+console.log("I");
+console.log("M");
+console.log("A");
+console.log("N");
+console.log("S");
+console.log("H");
+console.log("U");
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("M");
+    console.log("A");
+    console.log("N");
+    console.log("S");
+    console.log("H");
+    console.log("U");
+}
+sayMyName()
+
+function addTwoNumber(number1,number2){
+   console.log( number1+number2);
+}
+addTwoNumber(3,4)
