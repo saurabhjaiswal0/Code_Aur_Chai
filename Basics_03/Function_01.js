81c4c09e91a42ee369c0e7b359f2f0d1dd044fc7
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-13 19:28:52 +0000

    scope

--- Basics_03/Function_01.js
+++ Basics_03/Function_01.js
@@ -33,3 +33,27 @@ function loginUserMessage(username){
 }
 console.log(loginUserMessage());
 
+// **********************************************************
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200,400,600))
+const user={
+    username:"Saurabh",
+    price:199
+
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user)
+
+// *******************************************************
+
+const myNewArray=[200,400,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+
