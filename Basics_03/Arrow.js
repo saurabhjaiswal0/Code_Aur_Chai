1bfc515151d9a36d1c453dab666b61e8192fc3e6
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-14 11:43:15 +0000

    Arrow

--- Basics_03/Arrow.js
+++ Basics_03/Arrow.js
@@ -10,4 +10,33 @@ const user={
 // user.welcomeMessage()
 // user.username="Abhi"
 // user.welcomeMessage()
-console.log(this);
+
+// **********************************************//
+
+// function chai(){
+//     let username="Saurabh"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai=()=>{
+//     let username="Saurabh"
+//     console.log(this);
+// }
+// chai()
+
+
+// ******************Explicit Return Function***************
+// const addTwo=(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addTwo(3,4));
+
+// ******************Implicit Return Function*********************
+const addTwo = (num1,num2) => (num1+num2)
+
+console.log(addTwo(3,4));
+
+
+
+
