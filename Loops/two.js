78c15eea5558b2cb9353ea48cf7d6e0230701f32
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-15 10:37:58 +0000

    two

--- Loops/two.js
+++ Loops/two.js
@@ -13,3 +13,9 @@ while(arr<myArray.length){
     console.log(`value is ${myArray [arr]}`);
     arr = arr+1
 }
+
+let score=1
+do{
+  console.log(`Score is ${score}`);
+  score++
+}while(score<=10);
