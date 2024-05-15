f1da20c6cbfe0955ef658e3f5583c69177e554dd
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-15 10:15:04 +0000

    one

--- Loops/one.js
+++ Loops/one.js
@@ -1,5 +1,25 @@
 //  for ***********************
 
-for(let index=0; index<Array.length; index++){
-    const element=array[index];
+// for(let i=1; i<=10; i++){
+//     const element=i;
+//     if(element==5){
+//         console.log("5 is the best element");
+//     }
+//     console.log(element);
+// }
+1
+
+for(let i=1; i<=10; i++){
+    // console.log(`Outer loops value:${i}`);
+    for(let j=1; j<=10; j++){
+        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        // console.log(i + "*" +j+ "=" + i*j);
+    }
+}
+
+let myArray =["Batman","Flash","Sperman"]
+console.log(myArray.length);
+for(let index=0; index<myArray.length; index++){
+    const element=myArray[index];
+    console.log(element);
 }
