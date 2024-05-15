85a183ccb86b70d9d619cec062d0ca497a2deaba
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-15 12:07:57 +0000

    four

---
+++ Loops/four.js
@@ -0,0 +1,24 @@
+const myObject={
+    js:'javscript',
+    cpp:'c++',
+    swift:'swift by apple'
+}
+for (const key in myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`);   
+}
+
+const programming = ["js","Ruby","py","java"]
+for (const key in programming) {
+    //   console.log([key]);
+        
+    }
+
+    const map=new Map()
+    map.set('IN',"India")
+    map.set('USA',"United state of america")
+    map.set('FR',"France") 
+    for (const key in map) {
+        console.log(key)
+            
+        }
+      
