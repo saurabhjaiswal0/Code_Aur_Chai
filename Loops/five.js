5b0420efad8ef3dce718e54ec8be700e9d0f1088
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-15 14:31:15 +0000

    five

--- Loops/five.js
+++ Loops/five.js
@@ -1,5 +1,13 @@
 const coding=["Java","python","Cpp","Js"]
 
-coding.forEach(function (item){
-    console.log(item);
+// coding.forEach(function (item){
+//     console.log(item);
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item);
+// })
+
+coding.forEach((item,index,arr)=>{
+       console.log(item,index,arr);
 })
