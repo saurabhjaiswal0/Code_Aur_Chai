bb0d2563386fe3b00ce81c26e1e54ebfacf5a701
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-15 14:50:03 +0000

    five

--- Loops/five.js
+++ Loops/five.js
@@ -8,6 +8,34 @@ const coding=["Java","python","Cpp","Js"]
 //     console.log(item);
 // })
 
-coding.forEach((item,index,arr)=>{
-       console.log(item,index,arr);
+// coding.forEach((item,index,arr)=>{
+//        console.log(item,index,arr);
+// })
+
+const myCoding =[
+    {
+        languageName:"javaScript",
+        languageFileName:"js"
+    },
+
+    {
+        languageName:"JAVA",
+        languageFileName:"java"
+    },
+
+    {
+        languageName:"Python",
+        languageFileName:"py"
+    },
+
+    {
+        languageName:"ReactJs",
+        languageFileName:"rs"
+    }
+]
+myCoding.forEach((item) =>{
+    // console.log(item);
+    console.log(item.languageFileName);
 })
+
+
