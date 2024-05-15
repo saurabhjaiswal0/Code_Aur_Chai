bc006ef14ed1be6c4dc632fa72c38fb59e97399e
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-15 10:25:47 +0000

    one

--- Loops/one.js
+++ Loops/one.js
@@ -18,8 +18,19 @@ for(let i=1; i<=10; i++){
 }
 
 let myArray =["Batman","Flash","Sperman"]
-console.log(myArray.length);
+// console.log(myArray.length);
 for(let index=0; index<myArray.length; index++){
     const element=myArray[index];
-    console.log(element);
+    // console.log(element);
+}
+
+// Break and Continue****************
+
+for(let index=1; index<=20; index++){
+    if(index==5){
+        console.log(`Detected 5`);
+        continue
+    }  
+    console.log(`Value of i is ${index}`);
+   
 }
