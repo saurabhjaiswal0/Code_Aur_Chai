f97e5960679accde40f81acc44da44e4b8bd2a67
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-15 10:35:19 +0000

    two

--- Loops/two.js
+++ Loops/two.js
@@ -1,7 +1,15 @@
 // While loop*************
 
-let index=0
-while(index<=10){
-    console.log(`Value of index is ${index}`);
-    index=index+2
+// let index=0
+// while(index<=10){
+//     console.log(`Value of index is ${index}`);
+//     index=index+2
+// }
+
+let myArray=["Batman","Shaktiman","Superman"]
+
+let arr=0
+while(arr<myArray.length){
+    console.log(`value is ${myArray [arr]}`);
+    arr = arr+1
 }
