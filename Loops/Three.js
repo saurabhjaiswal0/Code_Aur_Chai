fb0b73630ff0d472a52e9748fb4a3f73632a9b3c
saurabhjaiswal0 <167260706+saurabhjaiswal0@users.noreply.github.com>
2024-05-15 11:38:02 +0000

    three

---
+++ Loops/Three.js
@@ -0,0 +1,26 @@
+// *********For Off**************
+const arr=[1,2,3,4]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings="Hello World"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// *********Map**********
+const map=new Map()
+map.set('IN',"India")
+map.set('USA',"United state of america")
+map.set('FR',"France")
+
+// console.log(map);
+
+const myObject={
+    game1:'NFS',
+    game2:'Spiderman'
+}
+// for (const [key,value] of myObject) {
+//     console.log(key ,':-',value);
+// }
